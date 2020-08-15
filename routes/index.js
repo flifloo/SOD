9c0c14a34b24e33afa4e595709ae1cc69b1d2b0e
flifloo <flifloo@gmail.com>
2020-08-15 12:55:21 +0200

    Link database on index page generation for sandwichs & sandwichs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 let express = require("express");
 let router = express.Router();
+let models = require("../models");
 
-/* GET home page. */
-router.get("/", (req, res) => {
-  res.render("index", { title: "SOD" });
+router.get("/", async (req, res) => {
+  let departments = await models.Department.findAll();
+  let sandwichs = await models.Sandwich.findAll();
+  res.render("index", { title: "SOD", departments: departments, sandwichs: sandwichs });
 });
 
 module.exports = router;
