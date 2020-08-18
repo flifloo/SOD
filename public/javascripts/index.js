13f2d6558f0653a4b98db3e8d6d76e62d4308e35
flifloo <flifloo@gmail.com>
2020-08-18 16:48:09 +0200

    Add locales to index

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,10 @@
 const commandAction = document.getElementById("command-action");
 const rmButton = document.getElementById("remove-command");
+const locals = {
+    command: document.querySelector("#command1>h2").innerHTML.replace(" 1", ""),
+    sandwich: document.querySelector("label[for='sandwich1']").innerHTML,
+    day: document.querySelector("label[for='day1']").innerHTML
+};
 
 function lastCommandId() {
     let list = document.querySelectorAll("div.command h2");
@@ -9,14 +14,14 @@ function lastCommandId() {
 document.getElementById("add-command").addEventListener("click", () => {
     let id = lastCommandId() + 1;
     commandAction.insertAdjacentHTML("beforebegin", `<div id="command${id}" class="command">
-    <h2>Command ${id}</h2>
+    <h2>${locals.command} ${id}</h2>
     <div class="field">
-        <label for="sandwich">Sandwich:</label>
-        <input id="sandwich" type="list" list="sandwich-list" name="sandwich${id}" autocomplete="off" required>
+        <label for="sandwich${id}">${locals.sandwich}</label>
+        <input id="sandwich${id}" type="list" list="sandwich-list" name="sandwich${id}" autocomplete="off" required>
     </div>
     <div class="field">
-        <label for="day">Day:</label>
-        <input id="day" type="date" name="date${id}" required>
+        <label for="day${id}">${locals.day}</label>
+        <input id="da${id}y" type="date" name="date${id}" required>
     </div>
 </div>`);
     document.getElementById("command"+lastCommandId()).scrollIntoView({behavior: "smooth"});
