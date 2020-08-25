4cc884d03bb2f9973feb37834e9818e5dd335f37
flifloo <flifloo@gmail.com>
2020-08-25 13:56:11 +0200

    Optimise reCaptcha for multiple reCaptcha page

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -21,3 +21,14 @@ dark.addEventListener("click", () => {
     else
         contact.classList.add("hide");
 });
+
+function cb(token) {
+    document.querySelectorAll("div.recaptcha.recaptcha-cb").forEach(el => {
+        el.classList.remove("recaptcha-cb");
+        let input = document.createElement("input");
+        input.setAttribute("type", "hidden");
+        input.setAttribute("name", "g-recaptcha-response");
+        input.setAttribute("value", token);
+        el.appendChild(input);
+    });
+}
